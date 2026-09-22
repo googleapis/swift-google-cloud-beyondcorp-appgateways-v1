40a64fb3727a447e9ec012a60a93e423abde52aa
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:42:28 -0400

    chore: update librarian w/ better enum docs (#1017)
    In this version of librarian we improve the generated documentation for
    `enum` so the possible evolution and usage of the enums is more obvious.
    For example, we recommend using `@unknown default:` so they get warnings
    when new values appear in the enum.

--- Sources/GoogleCloudBeyondCorpAppGatewaysV1/AppGateway.swift
+++ Sources/GoogleCloudBeyondCorpAppGatewaysV1/AppGateway.swift
@@ -249,6 +249,12 @@ public struct AppGateway: Codable, Equatable, GoogleWKT._AnyPackable,
 
   /// Enum containing list of all possible network connectivity options
   /// supported by BeyondCorp AppGateway.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum Type_: Codable, Equatable, Sendable {
     /// Default value. This value is unused.
     case unspecified
@@ -256,15 +262,21 @@ public struct AppGateway: Codable, Equatable, GoogleWKT._AnyPackable,
     case tcpProxy
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
@@ -347,6 +359,12 @@ public struct AppGateway: Codable, Equatable, GoogleWKT._AnyPackable,
   }
 
   /// Represents the different states of an AppGateway.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum State: Codable, Equatable, Sendable {
     /// Default value. This value is unused.
     case unspecified
@@ -363,15 +381,21 @@ public struct AppGateway: Codable, Equatable, GoogleWKT._AnyPackable,
     case down
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
@@ -475,6 +499,12 @@ public struct AppGateway: Codable, Equatable, GoogleWKT._AnyPackable,
 
   /// Enum containing list of all possible host types supported by BeyondCorp
   /// Connection.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum HostType: Codable, Equatable, Sendable {
     /// Default value. This value is unused.
     case unspecified
@@ -482,15 +512,21 @@ public struct AppGateway: Codable, Equatable, GoogleWKT._AnyPackable,
     case gcpRegionalMig
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
